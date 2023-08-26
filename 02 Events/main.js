5373fca2d36955de02cd8a0fff5a5d8d7ca46ad1
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-08-26 17:51:05 +0300

    Create product page.

---
+++ 02 Events/main.js
@@ -0,0 +1,45 @@
+// ------------ get page elements
+
+const createBtn = document.querySelector('#create-btn');
+const clearBtn = document.querySelector('#clear-btn');
+
+const nameInput = document.querySelector('#nameInput');
+const priceInput = document.querySelector('#priceInput');
+const tBody = document.querySelector('#product-table tbody');
+
+createBtn.onclick = () => {
+    //alert("Clicked");
+
+    // let item = {
+    //     id: 1,
+    //     name: nameInput.value,
+    //     price: priceInput.value
+    // };
+    let item = new Product(nameInput.value, priceInput.value);
+    
+    console.log(item);
+
+    addProductToTable(item);
+};
+
+clearBtn.onclick = () => {
+    tBody.innerHTML = '';
+}
+
+function addProductToTable(item) {
+    tBody.innerHTML += `<tr>
+                            <th scope="row">${item.id}</th>
+                            <td>${item.name}</td>
+                            <td>${item.price}$</td>
+                        </tr>`;
+}
+
+class Product {
+    static count = 10;
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        this.id = Product.count++;
+    }
+}
